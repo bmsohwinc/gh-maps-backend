18c4831549661e2b8876499184806b4ac90d3780
Sourabha Bharadwaj BM <bharasou58@gmail.com>
2024-05-04 00:47:42 +0530

    Add caching

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import cors from 'cors';
 import express from 'express';
-import {rateLimit} from 'express-rate-limit';
+import { rateLimit } from 'express-rate-limit';
+import NodeCache from 'node-cache';
+
 import 'dotenv/config';
 
 import { ClientError, gql, rawRequest } from 'graphql-request';
-import graphQLClient from './graphql-client.js';
 
 import pkg from 'body-parser';
 import { processGraphQLData } from './utils.js';
@@ -15,11 +16,11 @@ const BASE_URL = 'https://api.github.com/graphql';
 const PORT = 3001;
 const QUERY_RECORD_LIMIT = 20;
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
 })
 
 const app = express();
@@ -27,6 +28,10 @@ app.use(json())
 app.use(urlencoded({ extended: false }));
 app.use(limiter);
 app.use(cors());
+
+const CACHE_TTL_IN_SECS = 60 * 60 * 2; // 2 hours
+const followingsCache = new NodeCache({ stdTTL: CACHE_TTL_IN_SECS });
+const followersCache = new NodeCache({ stdTTL: CACHE_TTL_IN_SECS });
 
 
 const followingsGraphqlQuery = gql`
@@ -85,9 +90,17 @@ const followersGraphqlQuery = gql`
     } 
 `
 
-async function queryGitHub(req, res, queriedNode) {
+async function queryGitHub(req, res, query, queriedNode, cache) {
+    const cacheKey = req.query.login + '-' + req.query.afterPage;
+    const cacheData = cache.get(cacheKey);
 
-    const query = queriedNode === QUERIED_NODE.FOLLOWERS ? followersGraphqlQuery : followingsGraphqlQuery;
+    // Cache hit
+    if (cacheData !== undefined) {
+        console.log(`Cache hit for ${cacheKey}`);
+        res.send({ data: cacheData });
+        return;
+    }
+
     try {
         const variables = {
             limit: QUERY_RECORD_LIMIT,
@@ -95,28 +108,30 @@ async function queryGitHub(req, res, queriedNode) {
             afterPage: req.query.afterPage === 'null' ? null : req.query.afterPage,
         }
         const { data, errors, extensions, headers, status } = await rawRequest(
-            BASE_URL, 
-            query, 
+            BASE_URL,
+            query,
             variables,
             {
                 authorization: `Bearer ${process.env.GH_API_KEY}`,
             }
         );
         const requestsRemaining = headers.get('x-ratelimit-remaining');
-        res.send({ data: processGraphQLData(data, queriedNode) });
+        const processedData = processGraphQLData(data, queriedNode);
+        cache.set(cacheKey, processedData);
+        res.send({ data: processedData });
     } catch (error) {
         console.log(`User not found: ${req.query.login}`);
-        res.status(404).send({message: 'USER_NOT_FOUND'});
+        res.status(404).send({ message: 'USER_NOT_FOUND' });
     }
 }
 
 
 app.get('/followings', async (req, res, next) => {
-    await queryGitHub(req, res, QUERIED_NODE.FOLLOWINGS);
+    await queryGitHub(req, res, followingsGraphqlQuery, QUERIED_NODE.FOLLOWINGS, followingsCache);
 });
 
 app.get('/followers', async (req, res, next) => {
-    await queryGitHub(req, res, QUERIED_NODE.FOLLOWERS);
+    await queryGitHub(req, res, followersGraphqlQuery, QUERIED_NODE.FOLLOWERS, followersCache);
 });
 
 
